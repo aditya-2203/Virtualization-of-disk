d9cf39c74f6da4fc2275c5bdfe268e69efad7a7a
aditya <cs1130203@palasi.iitd.ernet.in>
2016-09-10 19:12:41 +0530

    add write api

--- Virtualization_3a_api.py
+++ Virtualization_3a_api.py
@@ -10,19 +10,41 @@ def read_data(data,block_no,offset,length):
 		return	
 	(disk_id,block_id)=disks.virtualblock_physical(block_no)
 	data=""
+	temp_byte=[]
 	if(disk_id==0):
 		for i in range(offset,offset+length):
-			data+=str(disks.disk_A[block_id][i]);
+			temp_byte.append(disks.disk_A[block_id][i]);
 	elif(disk_id==1):
 		for i in range(offset,offset+length):
-			data+=str(disks.disk_B[block_id][i]);
+			temp_byte.append(disks.disk_B[block_id][i]);
 	else:
 		print "invalid block no"
 		data=null;
-		return;			
+		return;
+	data="".join(map(chr,temp_byte))				
 	return data
 def write_data(block_no,offset,data):
-	return true
+	if(offset+len(data)>=100):
+		print "invalid length"
+		return False
+	if(block_no>=500):
+		print "invalid block number"
+		data=null
+		return False
+	(disk_id,block_id)=disks.virtualblock_physical(block_no)
+	length=len(data)
+	list_bytes=list(bytearray(data))
+	print list_bytes		
+	if(disk_id==0):
+		for i in range(offset,offset+length):
+			disks.disk_A[block_id][i]=list_bytes[i-offset]
+	elif(disk_id==1):
+		for i in range(offset,offset+length):
+			disks.disk_B[block_id][i]=list_bytes[i-offset]
+	else:
+		print "invalid block no"
+		return False;			
+	return True
 def create_disk(id,no_blocks):
 	return 	id
 def disk_read(id,block_no,offset,data):
